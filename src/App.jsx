73a2c4634182e7624adde91062cfb157fd684671
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-07-11 14:19:19 +0530

    Icon Controller Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import './App.css'
 import { Button } from './components/ui/button'
 import Header from './components/Header'
 import SideNav from './components/SideNav'
+import IconController from './components/IconController'
+import BackgroundController from './components/BackgroundController'
 
 function App() {
   const [count, setCount] = useState(0)
+  const[selectedIndex,setselectedIndex]=useState();
 
   return (
     <>
@@ -15,12 +18,17 @@ function App() {
         <Header/> 
 
       <div className='w-64 fixed'>
-        <SideNav selectedIndex={(value)=>console.log(value)}/> 
+        <SideNav selectedIndex={(value)=>setselectedIndex(value)}/> 
       </div>
 
         <div className='ml-64 grid grid-cols-1 md:grid-cols-6'>
-         <div className='md:col-span-2 bg-green-400'>
-            Control Pannel
+         <div className='md:col-span-2 border h-screen shadow-sm p-5'>
+          {selectedIndex==0?
+          <IconController/>:
+          <BackgroundController/> 
+        }
+           
+           
          </div>
          <div className='md:col-span-3 bg-red-500'>
             Icon Preview
